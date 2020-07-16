a5214d8302a208014d31c46983ade7361a507059
Mark Bentivegna <markbentivegna@gmail.com>
2020-07-16 08:50:46 -0400

    Renaming variables for clarity

--- mobility.py
+++ mobility.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-train_df = pd.read_csv('training_data.csv', encoding="ISO-8859-1")
+train_df = pd.read_csv('../training_data.csv', encoding="ISO-8859-1")
 matrix = train_df.groupby(['pickup_grid_number','dropoff_grid_number']).size().unstack().fillna(0)
 
 matrixByHours = train_df.groupby(['hour','pickup_grid_number','dropoff_grid_number']).size().unstack().fillna(0)
@@ -11,21 +11,21 @@ dropoff_matrix = train_df.groupby(['hour', 'dropoff_grid_number']).size().unstac
 pickup_matrix.fillna(0, inplace=True)
 dropoff_matrix.fillna(0, inplace=True)
 
-distance_obj = {}
+change_obj = {}
 for i in range(1, 2500):
     try:
         if i in dropoff_matrix[:].columns and i in pickup_matrix[:].columns:
-            distance_obj[str(i)] = dropoff_matrix[:][i] - pickup_matrix[:][i]
+            change_obj[str(i)] = dropoff_matrix[:][i] - pickup_matrix[:][i]
         elif i in dropoff_matrix[:].columns:
-            distance_obj[str(i)] = dropoff_matrix[:][i]
+            change_obj[str(i)] = dropoff_matrix[:][i]
         elif i in pickup_matrix[:].columns:
-            distance_obj[str(i)] = -pickup_matrix[:][i]
+            change_obj[str(i)] = -pickup_matrix[:][i]
 
     except KeyError:
         pass
 
 
-#distance_df is a 24x1010 (hour by zone) matrix containing the net change in a given zone
+#net_change_df is a 24x1010 (hour by zone) matrix containing the net change in a given zone
 #a positive value implies more people entering the zone then leaving
 #a negative value implies more people leaving the zone then entering
-distance_df = pd.DataFrame(distance_obj)
+net_change_df = pd.DataFrame(change_obj)
